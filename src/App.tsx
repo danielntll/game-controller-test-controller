8a8469ed6816052bf236d04f68f8d31e2c28d6d4
danielntll <116676924+danielntll@users.noreply.github.com>
2024-12-27 12:47:47 +0100

    fix multi input

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import { onAuthStateChanged, signInAnonymously, User } from "firebase/auth";
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
+  const [movement, setMovement] = useState<IJoystickUpdateEvent | null>(null);
+  const [btn1, setBtn1] = useState<boolean>(false);
+  const [btn2, setBtn2] = useState<boolean>(false);
+  const [btn3, setBtn3] = useState<boolean>(false);
+  const [btn4, setBtn4] = useState<boolean>(false);
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -30,32 +35,87 @@ function App() {
     });
   }, []);
 
-  const move = (joystick?: IJoystickUpdateEvent) => {
-    console.log("adding todo");
+  useEffect(() => {
+    if (user) {
+      uploadData();
+    }
+  }, [movement, btn1, btn2, btn3, btn4]);
+
+  const uploadData = () => {
     const dbRef = ref(database, "players/" + user?.uid);
     set(dbRef, {
-      joystick: joystick,
+      joystick: movement,
+      btn1: btn1,
+      btn2: btn2,
+      btn3: btn3,
+      btn4: btn4,
     });
   };
 
   return (
-    <>
+    <div className="mainContainer">
       <p>UID: {user?.uid}</p>
-      <Joystick
-        size={100}
-        sticky={false}
-        baseColor="red"
-        stickColor="blue"
-        move={(e) => {
-          move(e);
-          console.log("moved :: ", e);
-        }}
-        stop={(e) => {
-          console.log("stopped :: ", e);
-          move(e);
-        }}
-      ></Joystick>
-    </>
+      <div className="contollers">
+        <Joystick
+          size={100}
+          sticky={false}
+          baseColor="red"
+          stickColor="blue"
+          move={(e) => {
+            setMovement(e);
+            console.log("moved :: ", e);
+          }}
+          stop={(e) => {
+            console.log("stopped :: ", e);
+            setMovement(e);
+          }}
+        ></Joystick>
+        <div className="container">
+          <div className="top">
+            <button
+              type="button"
+              onMouseDown={() => setBtn1(true)}
+              onMouseUp={() => setBtn1(false)}
+              onTouchStart={() => setBtn1(true)}
+              onTouchEnd={() => setBtn1(false)}
+            >
+              ♺
+            </button>
+          </div>
+          <div className="center">
+            <button
+              type="button"
+              onMouseDown={() => setBtn2(true)}
+              onMouseUp={() => setBtn2(false)}
+              onTouchStart={() => setBtn2(true)}
+              onTouchEnd={() => setBtn2(false)}
+            >
+              ⚃
+            </button>
+            <button
+              type="button"
+              onMouseDown={() => setBtn3(true)}
+              onMouseUp={() => setBtn3(false)}
+              onTouchStart={() => setBtn3(true)}
+              onTouchEnd={() => setBtn3(false)}
+            >
+              ⚪
+            </button>
+          </div>
+          <div className="top">
+            <button
+              type="button"
+              onMouseDown={() => setBtn4(true)}
+              onMouseUp={() => setBtn4(false)}
+              onTouchStart={() => setBtn4(true)}
+              onTouchEnd={() => setBtn4(false)}
+            >
+              ⚔
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
